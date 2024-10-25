c93f4741d44acdd659411a9a9aea16dd4356cc82
bunuabhi2023 <asraijada08@gmail.com>
2024-10-25 22:20:27 +0530

    bug fix in hotel room

--- controllers/hotelRoomsController.js
+++ controllers/hotelRoomsController.js
@@ -139,7 +139,6 @@ exports.getAllMyHotelsRooms = catchError(async(req, res) =>{
 exports.getRoomsById = catchError(async(req, res) =>{
     const room = await HotelsRooms.findById(req.params.id)
     .populate('userId', 'name')
-    .populate('cityId', 'name')
     .populate('amenitiesId')
     .populate('roomCategoryId', 'name').exec();
     res.status(200).json({data:room});
@@ -147,7 +146,6 @@ exports.getRoomsById = catchError(async(req, res) =>{
 exports.getRoomsByHotel = catchError(async(req, res) =>{
     const rooms = await HotelsRooms.find({userId:req.params.id})
     .populate('userId', 'name')
-    .populate('cityId', 'name')
     .populate('amenitiesId')
     .populate('roomCategoryId', 'name').exec();
 
